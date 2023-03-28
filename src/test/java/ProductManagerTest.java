10aff6669b0050557f1c258d94b5a48c6234c29f
VCheckS <103640637+VCheckS@users.noreply.github.com>
2023-03-27 22:38:43 -0700

    Initial commit

---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,89 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductManagerTest {
+    @Test
+    public void searchByNameWhenFewProductFound() {
+
+
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+
+        Book book1 = new Book(1, "Harry Potter 1", "Rowilgs");
+        Book book2 = new Book(2, "Harry Poter 2", "Rowilgs2");
+        Book book3 = new Book(3, "Harry Pottter 3", "Rowilgs");
+        Smartphone phone1 = new Smartphone(1, "S10", "Samsung");
+        Smartphone phone2 = new Smartphone(2, "14 Pro", "Iphone");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(phone1);
+        repo.add(phone2);
+        Product[] actual = manager.searchBy("1");
+        Product[] expected = {book1, phone1, phone2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void searchByNameWhenNoProductFound() {
+
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+
+        Book book1 = new Book(1, "Harry Potter 1", "Rowilgs");
+        Book book2 = new Book(2, "Harry Poter 2", "Rowilgs2");
+        Book book3 = new Book(3, "Harry Pottter 3", "Rowilgs");
+        Smartphone phone4 = new Smartphone(4, "Iphone", "Apple");
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(phone4);
+        Product[] actual = manager.searchBy("x");
+        Product[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void searchByNameWhenOnePhoneFound() {
+
+        ProductRepository repo = new ProductRepository();
+        ProductManager manager = new ProductManager(repo);
+
+        Smartphone phone1 = new Smartphone(1, "Sone", "Samsung");
+        Smartphone phone2 = new Smartphone(2, "Stwo", "Samsung");
+        Smartphone phone3 = new Smartphone(3, "Sthre", "Samsung");
+        Smartphone phone4 = new Smartphone(4, "Iphone", "Apple");
+        Book book1 = new Book(1, "War and Peace", "Tolstoy");
+        repo.add(phone1);
+        repo.add(phone2);
+        repo.add(phone3);
+        repo.add(phone4);
+        Product[] actual = manager.searchBy("Ip");
+        Product[] expected = {phone4};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAdd() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager(repository);
+
+        Product product = new Product(1, "Smartphone");
+
+        manager.add(product);
+
+        assertEquals(1, repository.findAll().length);
+    }
+
+}
+
+
---
+++ src/test/java/ProductRepositoryTest.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ProductRepositoryTest {
+
+
+    @Test
+    public void removeById() {
+
+
+        ProductRepository repo = new ProductRepository();
+
+        Book book1 = new Book(1, "Harry Potter 1", "Rowilgs");
+        Book book2 = new Book(2, "Harry Poter 2", "Rowilgs2");
+        Book book3 = new Book(3, "Harry Pottter 3", "Rowilgs");
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.removeById(1);
+        Product[] actual = repo.findAll();
+        Product[] expected = {book2, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void removeByIdWhenIdIsCorrect() {
+
+
+        ProductRepository repo = new ProductRepository();
+
+        Book book1 = new Book(1, "Harry Potter 1", "Rowilgs");
+        Book book2 = new Book(2, "Harry Poter 2", "Rowilgs2");
+        Book book3 = new Book(3, "Harry Pottter 3", "Rowilgs");
+        Smartphone phone1 = new Smartphone(4, "Iphone", "Apple");
+        Smartphone phone2 = new Smartphone(5, "Iphoneten", "Apple");
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(phone1);
+        repo.add(phone2);
+
+        repo.removeById(5);
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, book3, phone1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+    @Test
+    public void removeByIdWhenIdIsIncorrect() {
+
+
+        ProductRepository repo = new ProductRepository();
+
+        Book book1 = new Book(1, "Harry Potter 1", "Rowilgs");
+        Book book2 = new Book(2, "Harry Poter 2", "Rowilgs2");
+        Book book3 = new Book(3, "Harry Pottter 3", "Rowilgs");
+        Smartphone phone1 = new Smartphone(4, "Iphone", "Apple");
+        Smartphone phone2 = new Smartphone(5, "Iphoneten", "Apple");
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(phone1);
+        repo.add(phone2);
+
+
+
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.removeById(6)
+                );
+
+
+    }
+
+}
